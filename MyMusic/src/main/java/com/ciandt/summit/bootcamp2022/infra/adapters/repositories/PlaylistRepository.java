2f0a304db6ccbdab5b075960173599408b974a93
LaizaZnCIT <laiza@ciandt.com>
2022-09-05 16:24:03 -0300

    throwing exception when playlist or music doesn't exist

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infra/adapters/repositories/PlaylistRepository.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infra/adapters/repositories/PlaylistRepository.java
@@ -2,10 +2,12 @@ package com.ciandt.summit.bootcamp2022.infra.adapters.repositories;
 
 import com.ciandt.summit.bootcamp2022.domain.Music;
 import com.ciandt.summit.bootcamp2022.domain.ports.repositories.PlaylistRepositoryPort;
+import com.ciandt.summit.bootcamp2022.exceptions.InvalidParameterException;
 import com.ciandt.summit.bootcamp2022.infra.adapters.entities.MusicEntity;
 import com.ciandt.summit.bootcamp2022.infra.adapters.entities.PlaylistEntity;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.stream.Collectors;
 
 @Component
@@ -18,18 +20,25 @@ public class PlaylistRepository implements PlaylistRepositoryPort {
     }
 
     @Override
-    public void addMusicToPlaylist(String playlistId, Music music) {
+    public void addMusicToPlaylist(String playlistId, Music music) throws EntityNotFoundException {
+        try {
         PlaylistEntity playlistEntity = this.springPlaylistRepository.getById(playlistId);
         playlistEntity.getMusics().add(new MusicEntity(music));
         this.springPlaylistRepository.save(playlistEntity);
+        } catch(EntityNotFoundException e) {
+            throw new EntityNotFoundException("Playlist ou música inexistentes.");
+        }
+
     }
 
     @Override
-    public void removeMusicFromPlaylist(String playlistId, String musicId) {
+    public void removeMusicFromPlaylist(String playlistId, String musicId) throws EntityNotFoundException {
+        try {
         PlaylistEntity playlistEntity = this.springPlaylistRepository.getById(playlistId);
         playlistEntity.setMusics(playlistEntity.getMusics().stream().filter(musicEntity -> !musicEntity.getId().equals(musicId)).collect(Collectors.toList()));
         this.springPlaylistRepository.save(playlistEntity);
+        } catch(EntityNotFoundException e) {
+            throw new EntityNotFoundException("Playlist ou música inexistentes.");
+        }
     }
-
-
 }
