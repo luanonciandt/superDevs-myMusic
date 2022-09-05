b09e80fc199ebc75d2d125141c0701591b549b5c
luanonciandt <luansena@ciandt.com>
2022-09-05 16:43:35 -0300

    test working well with security config class

--- MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/application/adapters/controllers/MusicControllerTest.java
+++ MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/application/adapters/controllers/MusicControllerTest.java
@@ -1,50 +1,55 @@
 package com.ciandt.summit.bootcamp2022.application.adapters.controllers;
 
+import com.ciandt.summit.bootcamp2022.domain.dtos.ArtistDTO;
+import com.ciandt.summit.bootcamp2022.domain.dtos.MusicDTO;
 import com.ciandt.summit.bootcamp2022.domain.ports.interfaces.MusicServicePort;
 import com.ciandt.summit.bootcamp2022.domain.ports.repositories.MusicRepositoryPort;
-import com.ciandt.summit.bootcamp2022.infra.config.BeanConfiguration;
-import com.ciandt.summit.bootcamp2022.infra.config.DbConfig;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-@ExtendWith({SpringExtension.class})
 @WebMvcTest(MusicController.class)
-@ContextConfiguration(classes = {DbConfig.class, BeanConfiguration.class, MusicServicePort.class, MusicRepositoryPort.class})
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@WebAppConfiguration
+@AutoConfigureMockMvc(addFilters = false)
 public class MusicControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @MockBean
-    private MusicServicePort musicServicePort;
+    private MusicServicePort musicService;
 
     @MockBean
     private MusicRepositoryPort musicRepositoryPort;
 
-    @BeforeEach
-    void initTest() throws Exception {
-        //given(interceptor.preHandle(any(), any(), any())).willReturn(true);
-    }
-
     @Test
     void findMusicByNameTest() throws Exception {
-        RequestBuilder request = MockMvcRequestBuilders.get("").param("name", "bru");
-        MvcResult result = mvc.perform(request).andReturn();
-        assertEquals(200, result.getResponse().getStatus());
+        List<MusicDTO> musicDTOS = new ArrayList<>();
+        musicDTOS.add(new MusicDTO("abcd-123", "Maracatu Atomico", new ArtistDTO("efgh-456", "Chico Science")));
+
+        Mockito.when(musicService.getMusicsByFilter("Maracatu")).thenReturn(musicDTOS);
+        mvc.perform(get("/api/musicas?name=Maracatu")).andExpect(MockMvcResultMatchers.status().isOk());
     }
 
 }
